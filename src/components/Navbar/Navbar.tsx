26fb9b3f71517d6e80aefa7e7b03e9b7827c57dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 10:27:49 +0300

    Deleted the Navbar component

--- src/components/Navbar/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const NavbarComponent = (props: Props) => {
-  return <nav className="">NavbarComponent</nav>;
-};
-
-export default NavbarComponent;
