7a02d91a212ee69aa11ed080ff6f1e72cb721bde
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 09:38:57 +0300

    Created the signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 type Props = {};
 
-const AuthPage = (props: Props) => {
+const SignUpPage = (props: Props) => {
   return <div>AuthPage</div>;
 };
 
-export default AuthPage;
+export default SignUpPage;
