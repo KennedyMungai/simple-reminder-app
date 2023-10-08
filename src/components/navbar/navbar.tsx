46c75b327aac963500883e317b776f18a5390a31
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 10:41:03 +0300

    Added some items to the navbar component

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 type Props = {};
 
 const NavBar = (props: Props) => {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <Logo />
+      <div className="">
+        <UserButton afterSignOutUrl="/" />
+        <ThemeSwitcher />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
