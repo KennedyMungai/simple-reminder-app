31d70c7871e68879e35a7051ea1a2c2df065f991
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 10:49:40 +0300

    Added some classes to the nav component

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,7 +7,7 @@ type Props = {};
 
 const NavBar = (props: Props) => {
   return (
-    <nav>
+    <nav className="flex w-full items-center justify-between p-4 px-8 h-[10vh]">
       <Logo />
       <div className="">
         <UserButton afterSignOutUrl="/" />
