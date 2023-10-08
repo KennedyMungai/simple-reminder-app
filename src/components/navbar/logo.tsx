05e6c60c3d8fd6f1951d65a7f03d75510c094fa6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 10:53:02 +0300

    Styled the h1 component

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Logo = (props: Props) => {
-  return <h1 className="text-2xl font-bold text-white">Simple Reminder</h1>;
+  return (
+    <h1 className="text-2xl font-bold text-white bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
+      SimpleReminder
+    </h1>
+  );
 };
 
 export default Logo;
