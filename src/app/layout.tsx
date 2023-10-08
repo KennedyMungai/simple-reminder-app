311ca3f37e72cacf4a8dfdac84fd5a11491ce22e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 10:39:14 +0300

    Imported the navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { Separator } from "@/components/ui/separator";
+import NavBar from "@/components/navbar/navbar";
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 
